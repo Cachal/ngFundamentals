ceedfc1ce5725bd067be46e38514b59f1684c4e9
Bhuvan Chhetri <bhuvanchhetri@gmail.com>
2019-05-29 21:05:46 -0400

    Routing to the same component, i.e. changing the events page based on the sessions

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {EventService} from '../shared/event.service';
-import {ActivatedRoute} from '@angular/router'
+import {ActivatedRoute,Params} from '@angular/router'
 import { IEvent, ISession } from '../shared';
 @Component({
     templateUrl:'./event-details.component.html',
@@ -22,8 +22,11 @@ export class EventDetailsComponent{
 
     }
     ngOnInit(){
-        this.event = this.eventService.getEvent
-            (+this.activatedRoute.snapshot.params['id']);
+        this.activatedRoute.params.forEach((params: Params) => {
+          this.event = this.eventService.getEvent(+params['id']);   
+        });
+        this.addMode= false;
+      
     }
     addSession(){
         this.addMode = true;
